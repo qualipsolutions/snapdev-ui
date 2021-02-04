d9469467533c97bc422c4153dabd74323f2fd91a
Tshepo Mgaga <tshepo.mgaga@gmail.com>
2021-02-04 22:52:10 +0200

    Updated

---
+++ src/components/Row/Row.stories.js
@@ -0,0 +1,29 @@
+import Grid from '@material-ui/core/Grid';
+import React from 'react';
+import DetailLabel from '../DetailLabel/DetailLabel';
+import Row from './Row';
+
+export default {
+  title: 'Layout/Row',
+  component: Row,
+};
+
+const Template = (args) => <Row {...args} />;
+
+export const Basic = Template.bind({});
+Basic.args = {
+  spacing: 10,
+  children: (
+    <>
+      <Grid item>
+        <DetailLabel label="App Name" text="Snapdev UI" />
+      </Grid>
+      <Grid item>
+        <DetailLabel label="Version" text="1.0.0" />
+      </Grid>
+      <Grid item>
+        <DetailLabel label="Author" text="Qualip Solutions" />
+      </Grid>
+    </>
+  ),
+};
