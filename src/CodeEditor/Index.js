d178bd642f4cc322ef382c1d5931f0a70dc9604b
Tshepo Mgaga <tshepo.mgaga@gmail.com>
2021-02-09 11:10:06 +0200

    Updated

--- src/CodeEditor/Index.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable prettier/prettier */
-
-// // Code Editor
-export { default as CodeMirror } from '../CodeMirror/CodeMirror';
-export { default as SfnEditor } from '../SfnEditor/SfnEditor';
-
