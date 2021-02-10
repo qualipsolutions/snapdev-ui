41a444d50e97cfd6242d17ca2216bcb4abc21e87
Tshepo Mgaga <tshepo.mgaga@gmail.com>
2021-02-10 08:46:59 +0200

    Updated

--- packages/snapdev-ui-docs/src/App.tsx
+++ packages/snapdev-ui-docs/src/App.tsx
@@ -1,12 +1,12 @@
-import { Text } from '@snapdev-ui/core'
-import React from 'react'
+import { Text } from "@snapdev-ui/core";
+import React from "react";
 
 function App() {
   return (
     <div>
       <Text>This component is working!!!</Text>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
