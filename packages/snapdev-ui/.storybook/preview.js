16f1de4e99713bcc9afa6fbbf1dce5ea987b762b
Tshepo Mgaga <tshepo.mgaga@gmail.com>
2021-02-11 12:15:17 +0200

    feat: Added a theme switcher

--- packages/snapdev-ui/.storybook/preview.js
+++ packages/snapdev-ui/.storybook/preview.js
@@ -1,21 +1,12 @@
-
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
 import { addDecorator } from '@storybook/react';
 import React from 'react';
+import KnobTheme from '../src/Theme/ThemeKnob';
+import { withKnobs } from '@storybook/addon-knobs';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
 
-const snapflowTheme = createMuiTheme({
-  palette: {
-    primary: {
-      light: '#757ce8',
-      main: '#3f50b5',
-      dark: '#002884',
-      contrastText: '#fff',
-    },
-  },
-});
-
-addDecorator(story => (<ThemeProvider theme={snapflowTheme}>{story()}</ThemeProvider>));
+addDecorator(withKnobs);
+addDecorator(story => (<KnobTheme><Paper style={{padding: 10}} elevation={0}>{story()}</Paper></KnobTheme>));
---
+++ packages/snapdev-ui/src/Theme/ThemeKnob.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { select } from '@storybook/addon-knobs';
+
+import Light from './Light';
+import Dark from './Dark';
+
+const themes = { Light, Dark };
+const themeNames = Object.keys(themes);
+
+export default ({ children }) => {
+  const theme = select('Theme', themeNames, themeNames[0], 'Themes');
+
+  return <ThemeProvider theme={themes[theme]}>{children}</ThemeProvider>;
+};
