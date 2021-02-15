83068ccf5cd11ec080d415e69424fab0b4df9d44
Tshepo Mgaga <tshepo@qualipsolutions.co.za>
2021-02-15 20:15:44 +0200

    feat: Updated TextField component

--- packages/snapdev-ui/src/TextField/TextField.stories.js
+++ packages/snapdev-ui/src/TextField/TextField.stories.js
@@ -10,34 +10,19 @@ const Template = (args) => <TextField {...args} />;
 
 export const Basic = Template.bind({});
 Basic.args = {
-  label: 'Workflow Name',
-  helperText:
-    'Alphanumeric characters, dashes, or underscores. i.e. my-business-process-workflow',
-};
-
-export const Placeholder = Template.bind({});
-Placeholder.args = {
-  label: 'First Name',
-  placeholder: 'Enter first name here',
-};
-
-export const FullWidth = Template.bind({});
-FullWidth.args = {
-  ...Placeholder.args,
   label: 'First Name',
   fullWidth: true,
 };
 
-export const HelperText = Template.bind({});
-HelperText.args = {
-  ...FullWidth.args,
+export const Hint = Template.bind({});
+Hint.args = {
+  ...Basic.args,
   helperText: 'Provide more details here',
 };
 
 export const Value = Template.bind({});
 Value.args = {
-  ...HelperText.args,
-  helperText: 'Provide more details here',
+  ...Hint.args,
   input: {
     value: 'Tshepo',
   },
@@ -45,8 +30,7 @@ Value.args = {
 
 export const Error = Template.bind({});
 Error.args = {
-  ...HelperText.args,
-  helperText: 'Provide more details here',
+  ...Hint.args,
   meta: {
     touched: true,
     error: 'Field is required',
